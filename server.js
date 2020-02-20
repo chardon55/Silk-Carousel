75835d0f4c37ef98ba1ca5dce8f83eaf3cdb7f6a
dy_55 <minidy20001001@outlook.com>
2020-02-20 12:09:45 +0800

    Deleted files

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(fs.readFileSync('demo.html'));
+    //res.write(fs.readFileSync('demo.html'));
     res.end();
 }).listen(port);
