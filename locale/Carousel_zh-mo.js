b662b1269978738128dddf7aee45794490e7b92c
dy_55 <minidy20001001@outlook.com>
2020-02-22 07:42:59 +0800

    Restore

---
+++ locale/Carousel_zh-mo.js
@@ -0,0 +1,23 @@
+﻿//絲緞輪播（版本：2.5.1） 繁體中文（中國澳門特別行政區）
+//Silk Carousel v2.5.1 Traditional Chinese (Macao SAR, China)
+
+lang = "zh-mo";
+
+initializing = "正在初始化……";
+noImage = "沒有圖片";
+
+learnMore = "了解更多";
+
+pause = "暫停";
+play = "播放";
+
+//錯誤信息
+cErr = "輪播發生錯誤：";
+
+errNoTarget = "沒有目標物件";
+
+stopWithErr = "輪播程式已中止";
+
+///
+
+//如果未找到相應國家或地區可以通知開發者製作相應的語言包，也可以根據需要自行製作臨時的js檔。
